12b20d60bdb9f6db1d088ff8058e722156415cd5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-12-20 09:57:43 +0900

    add xword.js

---
+++ xword.js/xword.js
@@ -0,0 +1,409 @@
+function shuffle(array) {
+	for(let i = array.length - 1; i > 0; i--){
+		let r = Math.floor(Math.random() * (i + 1));
+		let tmp = array[i];
+		array[i] = array[r];
+		array[r] = tmp;
+	}
+}
+function mid(text, i, ch) {
+	let ret = text.substr(0, i);
+	ret += ch;
+	ret += text.substr(i + 1);
+	return ret;
+}
+function set_cell(data, cx, cy, x, y, ch){
+	let text = data[y];
+	data[y] = mid(text, x, ch);
+	return data;
+}
+function corner_black(data, cx, cy){
+	return data[0][0] == '■' || data[cy - 1][0] == '■' ||
+	       data[cy - 1][cx - 1] == '■' || data[0][cx - 1] == '■';
+}
+function double_black(data, cx, cy){
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx - 1; ++x) {
+			if (data[y][x] == '■' && data[y][x + 1] == '■')
+				return true;
+		}
+	}
+	for (let x = 0; x < cx; ++x) {
+		for (let y = 0; y < cy - 1; ++y) {
+			if (data[y][x] == '■' && data[y + 1][x] == '■')
+				return true;
+		}
+	}
+	return false;
+}
+function tri_black_around(data, cx, cy){
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx; ++x) {
+			let count = 0;
+			if (0 < x && data[y][x - 1] == '■')
+				count += 1;
+			if (x + 1 < cx && data[y][x + 1] == '■')
+				count += 1;
+			if (0 < y && data[y - 1][x] == '■')
+				count += 1;
+			if (y + 1 < cy && data[y + 1][x] == '■')
+				count += 1;
+			if (count >= 3)
+				return true;
+		}
+	}
+	return false;
+}
+function divided_by_black(data, cx, cy) {
+	let ary = [];
+	ary.length = cx * cy;
+	let positions = [];
+	positions.push([0, 0]);
+	while (positions.length > 0) {
+		let pos = positions.shift();
+		let x = pos[0], y = pos[1];
+		if (!ary[y * cx + x]) {
+			ary[y * cx + x] = true;
+			if (0 < x && data[y][x - 1] != '■') {
+				positions.push([x - 1, y]);
+			}
+			if (0 < y && data[y - 1][x] != '■') {
+				positions.push([x, y - 1]);
+			}
+			if (x + 1 < cx && data[y][x + 1] != '■') {
+				positions.push([x + 1, y]);
+			}
+			if (y + 1 < cy && data[y + 1][x] != '■') {
+				positions.push([x, y + 1]);
+			}
+		}
+	}
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx; ++x) {
+			let i = y * cx + x;
+			if (!ary[i] && data[y][x] != '■')
+				return true;
+		}
+	}
+	return false;
+}
+function copy_board(data, cx, cy) {
+	let ary = [];
+	for (let y = 0; y < cy; ++y) {
+		ary.push(data[y] + '');
+	}
+	return ary;
+}
+function generate_empty_board(cx, cy) {
+	let ary = [];
+	for (let y = 0; y < cy; ++y) {
+		let text = '';
+		for (let x = 0; x < cx; ++x) {
+			text += '　';
+		}
+		ary.push(text);
+	}
+	return ary;
+}
+function generate_blacks(data, cx, cy) {
+	if (corner_black(data, cx, cy))
+		return null;
+	if (double_black(data, cx, cy))
+		return null;
+	if (tri_black_around(data, cx, cy))
+		return null;
+	if (divided_by_black(data, cx, cy))
+		return null;
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx; ++x) {
+			if (data[y][x] != '　')
+				continue;
+			let lo = x;
+			while (lo > 0) {
+				if (data[y][lo - 1] != '　')
+					break;
+				lo--;
+			}
+			while (x + 1 < cx) {
+				if (data[y][x + 1] != '　')
+					break;
+				x++;
+			}
+			let hi = x;
+			++x;
+			if (lo + 4 <= hi) {
+				let a = [0, 1, 2, 3];
+				shuffle(a);
+				for (let k = 0; k < 4; ++k) {
+					let copy = copy_board(data, cx, cy);
+					copy = set_cell(copy, cx, cy, lo + a[k], y, '■');
+					let ret = generate_blacks(copy, cx, cy);
+					if (ret) {
+						return ret;
+					}
+				}
+				return null;
+			}
+		}
+	}
+	for (let x = 0; x < cx; ++x) {
+		for (let y = 0; y < cy; ++y) {
+			if (data[y][x] != '　')
+				continue;
+			let lo = y;
+			while (lo > 0) {
+				if (data[lo - 1][x] != '　')
+					break;
+				lo--;
+			}
+			while (y + 1 < cy) {
+				if (data[y + 1][x] != '　')
+					break;
+				y++;
+			}
+			let hi = y;
+			++y;
+			if (lo + 4 <= hi) {
+				let a = [0, 1, 2, 3];
+				shuffle(a);
+				for (let k = 0; k < 4; ++k) {
+					let copy = copy_board(data, cx, cy);
+					copy = set_cell(copy, cx, cy, x, lo + a[k], '■');
+					let ret = generate_blacks(copy, cx, cy);
+					if (ret) {
+						return ret;
+					}
+				}
+				return null;
+			}
+		}
+	}
+	return data;
+}
+function get_candidates(dict, pattern, data, cx, cy) {
+	let patlen = pattern.length;
+	let cands = [];
+	for (let i = 0; i < dict.length; ++i) {
+		let word = dict[i][0];
+		let wordlen = word.length;
+		if (wordlen != patlen)
+			continue;
+		let matched = true;
+		for (let k = 0; k < wordlen; ++k) {
+			if (pattern[k] != '　' && pattern[k] != word[k]) {
+				matched = false;
+				break;
+			}
+		}
+		if (!matched)
+			continue;
+		cands.push(word);
+	}
+	return cands;
+}
+function any_candidate(dict, pattern) {
+	let patlen = pattern.length;
+	for (let i = 0; i < dict.length; ++i) {
+		let word = dict[i][0];
+		let wordlen = word.length;
+		if (wordlen != patlen)
+			continue;
+		let found = false;
+		for (let k = 0; k < wordlen; ++k) {
+			if (pattern[k] != '　') {
+				found = true;
+				if (pattern[k] != word[k])
+					break;
+			}
+		}
+		if (found)
+			return true;
+	}
+	return false;
+}
+function is_solution(data, cx, cy) {
+	if (corner_black(data, cx, cy))
+		return false;
+	if (double_black(data, cx, cy))
+		return false;
+	if (tri_black_around(data, cx, cy))
+		return false;
+	if (divided_by_black(data, cx, cy))
+		return false;
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx; ++x) {
+			if (data[y][x] == '　')
+				return false;
+		}
+	}
+	return true;
+}
+function solve_board_recurse(dict, data, cx, cy) {
+	if (corner_black(data, cx, cy)) {
+		return null;
+	}
+	if (double_black(data, cx, cy)) {
+		return null;
+	}
+	if (tri_black_around(data, cx, cy)) {
+		return null;
+	}
+	if (divided_by_black(data, cx, cy)) {
+		return null;
+	}
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx - 1; ++x) {
+			let ch1 = data[y][x], ch2 = data[y][x + 1];
+			if ((ch1 == '　' && ch2 != '■' && ch2 != '　') ||
+			    (ch1 != '　' && ch1 != '■' && ch2 == '　'))
+			{
+				let lo, hi;
+				lo = hi = x;
+				while (lo > 0) {
+					if (data[y][lo - 1] == '■')
+						break;
+					lo--;
+				}
+				while (hi + 1 < cx) {
+					if (data[y][hi + 1] == '■')
+						break;
+					hi++;
+				}
+				let pattern = '';
+				for (let k = lo; k <= hi; ++k) {
+					pattern += data[y][k];
+				}
+				let cands = get_candidates(dict, pattern, data, cx, cy);
+				if (cands) {
+					shuffle(cands);
+					for (let icand = 0; icand < cands.length; ++icand) {
+						let cand = cands[icand];
+						let copy = copy_board(data, cx, cy);
+						for (let k = lo; k <= hi; ++k) {
+							copy = set_cell(copy, cx, cy, k, y, cand[k - lo]);
+						}
+						let ret = solve_board_recurse(dict, copy, cx, cy);
+						if (ret) {
+							return ret;
+						}
+					}
+				}
+				return null;
+			}
+		}
+	}
+	for (let x = 0; x < cx; ++x) {
+		for (let y = 0; y < cy - 1; ++y) {
+			let ch1 = data[y][x], ch2 = data[y + 1][x];
+			if ((ch1 == '　' && ch2 != '■' && ch2 != '　') ||
+			    (ch1 != '　' && ch1 != '■' && ch2 == '　'))
+			{
+				let lo, hi;
+				lo = hi = y;
+				while (lo > 0) {
+					if (data[lo - 1][x] == '■')
+						break;
+					lo--;
+				}
+				while (hi + 1 < cy) {
+					if (data[hi + 1][x] == '■')
+						break;
+					hi++;
+				}
+				let pattern = '';
+				for (let k = lo; k <= hi; ++k) {
+					pattern += data[k][x];
+				}
+				let cands = get_candidates(dict, pattern, data, cx, cy);
+				if (cands) {
+					shuffle(cands);
+					for (let icand = 0; icand < cands.length; ++icand) {
+						let cand = cands[icand];
+						let copy = copy_board(data, cx, cy);
+						for (let k = lo; k <= hi; ++k) {
+							copy = set_cell(copy, cx, cy, x, k, cand[k - lo]);
+						}
+						let ret = solve_board_recurse(dict, copy, cx, cy);
+						if (ret) {
+							return ret;
+						}
+					}
+				}
+				return null;
+			}
+		}
+	}
+	if (is_solution(data, cx, cy)) {
+		return data;
+	}
+	return null;
+}
+function solve_board(dict, data, cx, cy) {
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx; ++x) {
+			let ch = data[y][x];
+			if (ch != '　' && ch != '■') {
+				return solve_board_recurse(dict, data, cx, cy);
+			}
+		}
+	}
+	for (let y = 0; y < cy; ++y) {
+		for (let x = 0; x < cx - 1; ++x) {
+			let ch1 = data[y][x], ch2 = data[y][x + 1];
+			if (ch1 == '　' && ch2 == '　') {
+				let lo, hi;
+				lo = hi = x;
+				while (lo > 0) {
+					if (data[y][lo - 1] == '■')
+						break;
+					lo--;
+				}
+				while (hi + 1 < cx) {
+					if (data[y][hi + 1] == '■')
+						break;
+					hi++;
+				}
+				let patlen = hi - lo + 1;
+				for (let iword = 0; iword < dict.length; ++iword) {
+					let word = dict[iword][0];
+					let wordlen = word.length;
+					if (wordlen != patlen)
+						continue;
+					let copy = copy_board(data, cx, cy);
+					for (let k = lo; k <= hi; ++k) {
+						copy = set_cell(copy, cx, cy, k, y, word[k - lo]);
+					}
+					return solve_board_recurse(dict, copy, cx, cy);
+				}
+			}
+		}
+	}
+	return null;
+}
+function main(dict, data, cx, cy) {
+	shuffle(dict);
+	if (0) {
+		return solve_board(dict, data, cx, cy);
+	} else if (0) {
+		data = generate_empty_board(cx, cy);
+		data = generate_blacks(data, cx, cy);
+		return data;
+	} else {
+		do {
+			data = generate_empty_board(cx, cy);
+			let copy = copy_board(data, cx, cy);
+			data = generate_blacks(copy, cx, cy);
+			data = solve_board(dict, data, cx, cy);
+		} while (!data);
+		return data;
+	}
+}
+self.addEventListener('message', function(e){
+	let dict = e.data.dict;
+	let data = e.data.data;
+	let cx = e.data.cx;
+	let cy = e.data.cy;
+	data = main(dict, data, cx, cy);
+	self.postMessage(data);
+	self.close();
+});
---
+++ xword.js/xword_board.js
@@ -0,0 +1,326 @@
+// NOTE: This needs jQuery UI.
+;(function($){
+	$.widget("custom.xword_board", {
+		options: {
+			cx: 7,
+			cy: 7,
+			change: function(event, cap){
+				console.log("change event: " + event.type);
+			},
+			data: null,
+			show_answer: false,
+		},
+		_create: function(){
+			if (this.options.text){
+				this.text(this.options.text);
+			} else {
+				
+				this._build();
+			}
+		},
+		_init: function(){
+		},
+		cx: function(){
+			return this.options.cx;
+		},
+		cy: function(){
+			return this.options.cy;
+		},
+		isData: function(value){
+			if (Array.isArray(value) && value.length > 0){
+				let len1 = value.length, len2 = value[0].length;
+				for (let i = 0; i < len1; ++i){
+					if (value[i].length != len2)
+						return false;
+				}
+				return true;
+			}
+			return false;
+		},
+		文字正規化: function(str){
+			let text = '';
+			for (let i = 0; i < str.length; ++i) {
+				if (str[i] == '#' || str[i] == '■') {
+					text += '■';
+				} else if (str[i] == ' ' || str[i] == '　') {
+					text += '　';
+				} else {
+					text += str[i];
+				}
+			}
+			text = text.toUpperCase();
+			text = 半角カナを全角に(text);
+			text = ひらがなをカタカナに(text);
+			text = 半角英数字を全角に(text);
+			text = 小文字を大文字に(text);
+			return text;
+		},
+		normalizeData: function(value){
+			if (value == undefined) {
+				value = this.options.data;
+			}
+			let ary = value.concat();
+			for (let i = 0; i < ary.length; ++i){
+				ary[i] = this.文字正規化(ary[i]);
+			}
+			return ary;
+		},
+		data: function(newValue){
+			if (newValue == undefined){
+				return this.options.data;
+			} else {
+				if (this.isData(newValue)){
+					this.options.data = this.normalizeData(newValue);
+					this.options.cx = newValue[0].length;
+					this.options.cy = newValue.length;
+					this._build();
+				}
+			}
+		},
+		text: function(newValue){
+			if (newValue == undefined){
+				return this._data2text(this.data());
+			} else {
+				this.data(this._text2data(newValue));
+			}
+		},
+		_data2text: function(data){
+			let ary = data.concat();
+			let text = ary.join("┃\n┃");
+			let cx = ary[0].length;
+			let bar = Array(cx + 1).join("━");
+			let ret = "┏" + bar + "┓\n┃" + text + "┃\n┗" + bar + "┛\n";
+			return ret;
+		},
+		_text2data: function(text){
+			let data = text.join("\n");
+			data = data.replace(/━/g, "");
+			data = data.replace(/┏┓\n┃/g, '');
+			data = data.replace(/┃\n┗┛/g, '');
+			data = data.replace(/┃\n┃/g, "\n");
+			return data.split("\n");
+		},
+		cell: function(x, y, ch){
+			let self = this;
+			if (self.options.data && x >= 0 && y >= 0 &&
+			    y < self.options.data.length &&
+			    x < self.options.data[0].length)
+			{
+				if (ch == undefined){
+					ch = $("#" + this._get_id(x, y) + " input").val();
+					if (ch == '' || ch == ' ' || ch == '　') {
+						ch = '　';
+					}
+					return ch;
+				} else {
+					ch = this.文字正規化(ch);
+					if (ch == '■' || ch == '#'){
+						ch = '■';
+						$("#" + this._get_id(x, y)).addClass('xword_board-cell-black');
+						$("#" + this._get_id(x, y)).removeClass('xword_board-cell-white');
+						$("#" + this._get_id(x, y)).removeClass('xword_board-cell-red');
+						$("#" + this._get_id(x, y)).removeClass('xword_board-cell-yellow');
+						$("#" + this._get_id(x, y) + " input").val(ch);
+					} else {
+						$("#" + this._get_id(x, y)).addClass('xword_board-cell-white');
+						$("#" + this._get_id(x, y)).removeClass('xword_board-cell-black');
+						$("#" + this._get_id(x, y)).removeClass('xword_board-cell-red');
+						$("#" + this._get_id(x, y)).removeClass('xword_board-cell-yellow');
+						if (ch == '' || ch == ' ' || ch == '　') {
+							$("#" + this._get_id(x, y) + " input").val('');
+						} else {
+							$("#" + this._get_id(x, y) + " input").val(ch);
+						}
+					}
+				}
+			}
+		},
+		mark: function(x, y, mark){
+			let self = this;
+			if (self.options.data && x >= 0 && y >= 0 &&
+			    y < self.options.data.length &&
+			    x < self.options.data[0].length)
+			{
+				if (mark == undefined){
+					return $("#" + this._get_id(x, y) + " .xword_board-small-text-mark").text();
+				} else {
+					$("#" + this._get_id(x, y) + " .xword_board-small-text-mark").text(mark);
+				}
+			}
+		},
+		number: function(x, y, num){
+			let self = this;
+			if (self.options.data && x >= 0 && y >= 0 &&
+			    y < self.options.data.length &&
+			    x < self.options.data[0].length)
+			{
+				if (num == undefined){
+					return $("#" + this._get_id(x, y) + " .xword_board-small-text-number").text();
+				} else {
+					$("#" + this._get_id(x, y) + " .xword_board-small-text-number").text(num);
+				}
+			}
+		},
+		_build: function(){
+			let self = this;
+			let id = self.element.prop("id");
+			if (!id){
+				id = "the-xword-board-" + math.random();
+				self.element.prop("id", id);
+			}
+			let html = '';
+			if (self.options.data){
+				let data = self.options.data;
+				html += '<table class="xword_board-table">';
+				html += '  <tbody>';
+				let cx = this.options.cx, cy = this.options.cy;
+				for(let y = 0; y < cy; ++y){
+					html += '<tr>';
+					for(let x = 0; x < cx; ++x){
+						html += '<td id="' + self._get_id(x, y) + '" ';
+						let ch = data[y][x];
+						if (ch == '■' || ch == '#'){
+							html += 'class="xword_board-cell xword_board-cell-black">';
+						} else {
+							html += 'class="xword_board-cell xword_board-cell-white">';
+						}
+						html += '<div class="xword_board-small-text-number"></div>';
+						html += '<div class="xword_board-small-text-mark"></div>';
+						if (ch == '■' || ch == '#'){
+							html += '<input type="text" class="xword_board-textbox" value="■" maxlength="1" />';
+						} else if (ch == '' || ch == '　' || ch == ' ' || !this.options.show_answer) {
+							html += '<input type="text" class="xword_board-textbox" value="" maxlength="1" />';
+						} else {
+							html += '<input type="text" class="xword_board-textbox" value="' + ch + '" maxlength="1" />';
+						}
+						html += '</td>';
+					}
+					html += '</tr>';
+				}
+				html += '  </tbody>';
+				html += '</table>';
+			}
+			self.element.html(html);
+			$(".xword_board-textbox").blur(function(){
+				self.updateTextBoxes();
+			});
+		},
+		updateTextBoxes: function(){
+			let cx = this.options.cx, cy = this.options.cy;
+			for(let y = 0; y < cy; ++y){
+				let text = '';
+				for(let x = 0; x < cx; ++x){
+					let ch = $("#" + this._get_id(x, y) + " input").val();
+					if (ch == '') {
+						ch = '　';
+					}
+					ch = this.文字正規化(ch);
+					text += ch;
+					if (ch == '' || ch == ' ' || ch == '　') {
+						$("#" + this._get_id(x, y) + " input").val('');
+					} else {
+						$("#" + this._get_id(x, y) + " input").val(ch);
+					}
+				}
+			}
+		},
+		_get_id: function(x, y){
+			var id = this.element.prop("id");
+			return id + "-" + x + "-" + y;
+		},
+		cell_needs_number: function(x, y){
+			let data = this.options.data;
+			let cx = this.options.cx, cy = this.options.cy;
+			if (data[y][x] == '■') {
+				return false;
+			}
+			if (x == 0 || data[y][x - 1] == '■') {
+				if (x + 1 < cx && data[y][x + 1] != '■') {
+					return true;
+				}
+			}
+			if (y == 0 || data[y - 1][x] == '■') {
+				if (y + 1 < cy && data[y + 1][x] != '■') {
+					return true;
+				}
+			}
+			return false;
+		},
+		cell_needs_number_x: function(x, y){
+			let data = this.options.data;
+			let cx = this.options.cx;
+			if (data[y][x] == '■') {
+				return false;
+			}
+			if (x == 0 || data[y][x - 1] == '■') {
+				if (x + 1 < cx && data[y][x + 1] != '■') {
+					return true;
+				}
+			}
+			return false;
+		},
+		cell_needs_number_y: function(x, y){
+			let data = this.options.data;
+			let cy = this.options.cy;
+			if (data[y][x] == '■') {
+				return false;
+			}
+			if (y == 0 || data[y - 1][x] == '■') {
+				if (y + 1 < cy && data[y + 1][x] != '■') {
+					return true;
+				}
+			}
+			return false;
+		},
+		do_numbering: function() {
+			let data = this.options.data;
+			let cx = this.options.cx, cy = this.options.cy;
+			let number = 1;
+			for (let y = 0; y < cy; ++y) {
+				for (let x = 0; x < cx; ++x) {
+					if (this.cell_needs_number(x, y)) {
+						this.number(x, y, number);
+						number += 1;
+					}
+				}
+			}
+		},
+		show_answer: function(newValue) {
+			if (newValue == undefined) {
+				return this.options.show_answer;
+			} else {
+				this.options.show_answer = newValue;
+				let data = this.options.data;
+				let cx = this.options.cx, cy = this.options.cy;
+				if (newValue) {
+					for (let y = 0; y < cy; ++y) {
+						for (let x = 0; x < cx; ++x) {
+							let ch = this.cell(x, y);
+							ch = this.文字正規化(ch);
+							let wrong = (data[y][x] != ch);
+							if (wrong) {
+								this.cell(x, y, data[y][x]);
+								$("#" + this._get_id(x, y)).addClass('xword_board-cell-red');
+								$("#" + this._get_id(x, y)).removeClass('xword_board-cell-white');
+								$("#" + this._get_id(x, y)).removeClass('xword_board-cell-black');
+							} else if (data[y][x] != '■') {
+								$("#" + this._get_id(x, y)).addClass('xword_board-cell-yellow');
+								$("#" + this._get_id(x, y)).removeClass('xword_board-cell-white');
+								$("#" + this._get_id(x, y)).removeClass('xword_board-cell-black');
+							}
+							$("#" + this._get_id(x, y) + " input").prop('disabled', true);
+						}
+					}
+				} else {
+					for (let y = 0; y < cy; ++y) {
+						for (let x = 0; x < cx; ++x) {
+							if (data[y][x] != '■') {
+								this.cell(x, y, '');
+							}
+						}
+					}
+				}
+			}
+		},
+	});
+})(jQuery);
