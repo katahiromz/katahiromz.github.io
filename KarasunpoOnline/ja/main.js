e3c7c00cd7363cb0b764739c11563f5feeff4a6f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-09-05 08:19:15 +0900

    0.8959

--- KarasunpoOnline/ja/main.js
+++ KarasunpoOnline/ja/main.js
@@ -2,7 +2,7 @@
 // Copyright (C) 2021 Katayama Hirofumi MZ. All Rights Reserved.
 // License: MIT
 
-var KARASUNPO_VERSION = "0.8958"; // カラスンポのバージョン番号。
+var KARASUNPO_VERSION = "0.8959"; // カラスンポのバージョン番号。
 
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
