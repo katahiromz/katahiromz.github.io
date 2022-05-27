71509c9dc243a912639cffc3a80c85d9d07b6cb1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-05-27 12:56:22 +0900

    update

--- saimin/mic.js
+++ saimin/mic.js
@@ -1,27 +1,10 @@
 var mic_context = null;
-var mic_distortion = null;
 var mic_gainNode = null;
 var mic_biquadFilter = null;
 var mic_source = null;
 var mic_analyser = null;
 
-// distortion curve for the waveshaper, thanks to Kevin Ennis
-// http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
-function makeDistortionCurve(amount){
-  let k = typeof amount === 'number' ? amount : 50,
-    n_samples = 44100,
-    curve = new Float32Array(n_samples),
-    deg = Math.PI / 180,
-    i = 0,
-    x;
-  for ( ; i < n_samples; ++i ){
-    x = i * 2 / n_samples - 1;
-    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
-  }
-  return curve;
-}
-
-function mic_Setup(){
+function mic_setup(){
   if (navigator.mediaDevices === undefined){
     navigator.mediaDevices = {};
   }
@@ -46,7 +29,6 @@ function mic_connect(){
   mic_analyser.maxDecibels = -10;
   mic_analyser.smoothingTimeConstant = 0.85;
 
-  mic_distortion = mic_context.createWaveShaper();
   mic_gainNode = mic_context.createGain();
   mic_biquadFilter = mic_context.createBiquadFilter();
 
@@ -56,12 +38,11 @@ function mic_connect(){
     .then(
       function(stream){
         mic_source = mic_context.createMediaStreamSource(stream);
-        mic_source.connect(mic_distortion);
-        mic_distortion.connect(mic_biquadFilter);
+        mic_source.connect(mic_biquadFilter);
         mic_biquadFilter.connect(mic_gainNode);
         mic_gainNode.connect(mic_analyser);
         mic_analyser.connect(mic_context.destination);
-        mic_voiceChange();
+        mic_biquadFilter.gain.setTargetAtTime(0, mic_context.currentTime, 0)
     })
     .catch(function(err){
       console.log('The following gUM error occured: ' + err);
@@ -69,43 +50,25 @@ function mic_connect(){
   } else {
      console.log('getUserMedia not supported on your browser!');
   }
+
+  mic_gainNode.value = 2.0;
+  mic_biquadFilter.gain.setTargetAtTime(0, mic_context.currentTime, 0)
+  mic_biquadFilter.disconnect(0);
+  mic_biquadFilter.connect(mic_gainNode);
+  mic_biquadFilter.type = "lowshelf";
+  mic_biquadFilter.frequency.setTargetAtTime(1000, mic_context.currentTime, 0)
+  mic_biquadFilter.gain.setTargetAtTime(25, mic_context.currentTime, 0)
 }
 
 function mic_disconnect(){
   mic_biquadFilter.disconnect(0);
   mic_source.disconnect(0);
-  mic_distortion.disconnect(0);
   mic_biquadFilter.disconnect(0);
   mic_gainNode.disconnect(0);
   mic_analyser.disconnect(0);
   mic_context = null;
-  mic_distortion = null;
   mic_gainNode = null;
   mic_biquadFilter = null;
   mic_source = null;
   mic_analyser = null;
-}
-
-function mic_voiceChange(voiceSetting){
-  mic_distortion.oversample = '4x';
-  mic_biquadFilter.gain.setTargetAtTime(0, mic_context.currentTime, 0)
-
-  switch (voiceSetting){
-  case 'distortion':
-    mic_biquadFilter.disconnect(0);
-    mic_biquadFilter.connect(mic_gainNode);
-    mic_distortion.curve = makeDistortionCurve(400);
-    break;
-  case 'biquad':
-    mic_biquadFilter.disconnect(0);
-    mic_biquadFilter.connect(mic_gainNode);
-    mic_biquadFilter.type = "lowshelf";
-    mic_biquadFilter.frequency.setTargetAtTime(1000, mic_context.currentTime, 0)
-    mic_biquadFilter.gain.setTargetAtTime(25, mic_context.currentTime, 0)
-    break;
-  case 'off':
-    mic_biquadFilter.disconnect(0);
-    mic_biquadFilter.connect(mic_gainNode);
-    break;
-  }
 }
