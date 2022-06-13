beffbb9de2e8b260bacb0c3b33ad23fdf177aef1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-06-13 14:38:53 +0900

    3.1.0

--- SaiminOnTheWeb/main.js
+++ SaiminOnTheWeb/main.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 8 */
 jQuery(function($){
 	const NUM_TYPE = 5;
-	const VERSION = '3.0.9';
+	const VERSION = '3.1.0';
 	const DEBUG = true;
 	var cx = 0, cy = 0;
 	var old_cx = null, old_cy = null;
@@ -359,6 +359,7 @@ jQuery(function($){
 
 		$("#sound-button").click(function(){
 			if (soundName != ''){
+				console.log("sn/" + soundName + ".mp3");
 				let sound = new Audio("sn/" + soundName + ".mp3");
 				sound.play();
 			}else{
--- SaiminOnTheWeb/sw.js
+++ SaiminOnTheWeb/sw.js
@@ -1,6 +1,6 @@
 // configuration
 const
-  version = '3.0.9',
+  version = '3.1.0',
   CACHE = version + '::PWAsite',
   offlineURL = 'index.html',
   installFilesEssential = [
@@ -99,6 +99,7 @@ self.addEventListener('fetch', event => {
       return;
     }
   }
+
   event.respondWith(
     caches.open(CACHE)
       .then(cache => {
