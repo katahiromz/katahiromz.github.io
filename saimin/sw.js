4de6cc1bb78e239b7a819bde855345a5502cb1b5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-06-14 08:56:55 +0900

    update sw.js

--- saimin/sw.js
+++ saimin/sw.js
@@ -3,19 +3,19 @@ importScripts(['service-worker.js']);
 let sw = new ServiceWorkerJS();
 
 sw.precache('precached', [
-	'/',
-	'/index.html',
-	'/complex.min.js',
-	'/copyright.js',
-	'/favicon.ico',
-	'/icons/icon-192.png',
-	'/jquery-3.6.0.min.js',
-	'/jquery-ui.css',
-	'/jquery-ui.min.js',
-	'/main.js',
-	'/manifest.json',
-	'/mic.js',
-	'/style.css',
+	'./',
+	'./index.html',
+	'./complex.min.js',
+	'./copyright.js',
+	'./favicon.ico',
+	'./icons/icon-192.png',
+	'./jquery-3.6.0.min.js',
+	'./jquery-ui.css',
+	'./jquery-ui.min.js',
+	'./main.js',
+	'./manifest.json',
+	'./mic.js',
+	'./style.css',
 ]);
 
 sw.addRoute(/\.mp3$/, { method: 'get' }, sw.networkOnly({ timeout: 3000 }));
