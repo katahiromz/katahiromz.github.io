51d6462a1faeeac8982931176541200cc18e59c8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-06-14 11:49:28 +0900

    update sw.js

--- SaiminOnTheWeb/sw.js
+++ SaiminOnTheWeb/sw.js
@@ -18,4 +18,17 @@ sw.precache('precached', [
 	'./style.css',
 ]);
 
-sw.addRoute(/\.mp3$/, { method: 'get' }, sw.networkOnly({ timeout: 3000 }));
+sw.addRoute(/.*\.mp3$/, { method: 'get' }, sw.networkOnly({ timeout: 3000 }));
+
+sw.addRoute('./images/ui-bg_glass_40_ffc73d_1x400.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-bg_highlight-hard_20_0972a5_1x100.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-bg_highlight-soft_33_003147_1x100.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-bg_highlight-soft_35_222222_1x100.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-bg_highlight-soft_44_444444_1x100.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-bg_highlight-soft_80_eeeeee_1x100.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-bg_loop_25_000000_21x21.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-icons_222222_256x240.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-icons_4b8e0b_256x240.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-icons_a83300_256x240.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-icons_cccccc_256x240.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
+sw.addRoute('./images/ui-icons_ffffff_256x240.png', { method: 'get' }, sw.cacheFirst({ cache: 'postcached' }));
