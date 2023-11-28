f5be7eea1feea76b3e82244e6917067850979a12
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-11-29 08:51:09 +0900

    improve face2 4

--- face2/app.js
+++ face2/app.js
@@ -1,89 +1,100 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d', { alpha: false, willReadFrequently: true });
-  let isAudioEnabled = false;
-  let isFrontCamera = false;
+	const video = document.getElementById('video');
+	const canvas = document.getElementById('canvas');
+	const ctx = canvas.getContext('2d', { alpha: false, willReadFrequently: true });
+	let isAudioEnabled = false;
+	let isFrontCamera = false;
+	let isVideoInit = false;
+	let anime = null;
 
-  // カメラが取得できたときに呼び出される関数。
-  const gotCamera = (stream) => {
-    video.srcObject = stream;
-  };
+	// カメラの制約を取得する関数。
+	const getCameraConstraints = () => {
+		if (isFrontCamera) {
+			return {
+				video: {
+					facingMode: 'user',
+				},
+				audio: isAudioEnabled,
+			};
+		} else {
+			return {
+				video: {
+					facingMode: { exact: 'environment' },
+				},
+				audio: isAudioEnabled,
+			};
+		}
+	};
 
-  // カメラの制約を取得する関数。
-  const getCameraConstraints = () => {
-    if (isFrontCamera) {
-      return {
-        video: {
-          facingMode: 'user',
-        },
-        audio: isAudioEnabled,
-      };
-    } else {
-      return {
-        video: {
-          facingMode: { exact: 'environment' },
-        },
-        audio: isAudioEnabled,
-      };
-    }
-  };
+	// カメラストリームを取得するメソッド
+	const getCameraStream = async () => {
+		return navigator.mediaDevices.getUserMedia(getCameraConstraints());
+	};
 
-  // カメラにアクセスするためのメソッド
-  const initCamera = async () => {
-    video.srcObject = null;
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(getCameraConstraints());
-      gotCamera(stream);
-    } catch (error) {
-      console.log(error);
-      try {
-        isFrontCamera = !isFrontCamera;
-        const stream = await navigator.mediaDevices.getUserMedia(getCameraConstraints());
-        gotCamera(stream);
-      } catch (error) {
-        console.error('Error accessing the camera:', error);
-      }
-    }
-  };
+	// カメラが取得できたときの処理。
+	const gotCamera = (stream) => {
+		video.srcObject = stream;
+		if (!isVideoInit) {
+			video.addEventListener('loadedmetadata', () => {
+				canvas.width = video.videoWidth;
+				canvas.height = video.videoHeight;
+				anime = requestAnimationFrame(processVideo);
+			});
+			isVideoInit = true;
+		}
+	};
 
-  // カメラ映像を加工するメソッド
-  const processVideo = () => {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+	const initCamera = async () => {
+		try {
+			const stream = await getCameraStream();
+			gotCamera(stream);
+		} catch (error) {
+			console.log('Error accessing the camera:', error);
+			try {
+				isFrontCamera = !isFrontCamera;
+				const stream = await getCameraStream();
+				gotCamera(stream);
+			} catch (error) {
+				console.error('Error accessing the camera:', error);
+			}
+		}
+	};
 
-    // ここに加工処理を追加
-    applyGrayscaleFilter();
+	// カメラ映像を加工するメソッド
+	const processVideo = () => {
+		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    // 加工された映像を表示
-    requestAnimationFrame(processVideo);
-  };
+		// ここに加工処理を追加
+		applyGrayscaleFilter();
 
-  // グレースケールフィルターを適用するメソッド
-  const applyGrayscaleFilter = () => {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
+		// 加工された映像を表示
+		if (anime)
+			anime = requestAnimationFrame(processVideo);
+	};
 
-    for (let i = 0; i < data.length; i += 4) {
-      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      data[i] = avg;
-      data[i + 1] = avg;
-      data[i + 2] = avg;
-    }
+	// グレースケールフィルターを適用するメソッド
+	const applyGrayscaleFilter = () => {
+		const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		const data = imageData.data;
 
-    ctx.putImageData(imageData, 0, 0);
-  };
+		for (let i = 0; i < data.length; i += 4) {
+			const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+			data[i] = avg;
+			data[i + 1] = avg;
+			data[i + 2] = avg;
+		}
 
-  sai_id_button_side.addEventListener('click', () => {
-    isFrontCamera = !isFrontCamera;
-    initCamera();
-  });
+		ctx.putImageData(imageData, 0, 0);
+	};
 
-  video.addEventListener('loadedmetadata', () => {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    processVideo();
-  });
+	sai_id_button_side.addEventListener('click', () => {
+		if (anime)
+			cancelAnimationFrame(anime);
+		isFrontCamera = !isFrontCamera;
+		video.srcObject = null;
+		initCamera();
+	});
 
-  // カメラの初期化
-  initCamera();
+	// カメラの初期化
+	initCamera();
 });
